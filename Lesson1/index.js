7a433c3252649d09b90d875869bf9f9817963a50
Nathan Eldridge <neldridg@gmail.com>
2020-07-28 18:40:56 -0500

    Added comments to JS code

--- Lesson1/index.js
+++ Lesson1/index.js
@@ -1,8 +1,15 @@
+// wait for the DOM to signal that it's done loading
 document.addEventListener('DOMContentLoaded', function() { 
+    // find the toggle button
     let toggleButton = document.getElementById('nav-toggle');
+
+    // add an event listener for 'click' events
     toggleButton.addEventListener('click', function() {
+
+        // get all the nav items
         let listItems = document.getElementsByClassName('nav-list-item');
-        console.log(listItems);
+
+        // For each class item, toggle the hidden class
         for(let item of listItems) {
             item.classList.toggle('hidden');
         }
